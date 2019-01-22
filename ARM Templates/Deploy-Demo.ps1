9e8bb40300433284856d84d09f9429eddf5728b9
Andreas Sobczyk <aso@coretech.dk>
2019-01-22 16:03:14 +0100

    readme added

--- ARM Templates/Deploy-Demo.ps1
+++ ARM Templates/Deploy-Demo.ps1
@@ -1,14 +1,40 @@
 #Login to Azure Resource Manager
 Login-AzureRmAccount
 
-############# Task 1 ###############
+#Validate your Azure Subscription
+Get-AzureRmContext
+
+#If need change subscription
+Get-AzureRmSubscription | Out-GridView -PassThru | Select-AzureRmSubscription
+
+############# Task 1 ############################################################################
+###  Deploy Storage Account
+############# Task 1 ############################################################################
 
 #Create Resource Group for Storage Account
 New-AzureRmResourceGroup -Name "StorageAccount-tst" -Location "WestEurope"
 
 #Deploy Storage Account
-New-AzureRmResourceGroupDeployment -StorageAccountPrefix "#StoragePrefix#" -Name "deployStorage-$(Get-Date -Format 'yyyMMddhhmmss')" `
-    -ResourceGroupName 'StorageAccount-tst' -TemplateFile .\2_StorageAccount.json
+New-AzureRmResourceGroupDeployment -StorageAccountPrefix "AsoTesting" -Name "deployStorage-$(Get-Date -Format 'yyyMMddhhmmss')" `
+    -ResourceGroupName 'StorageAccount-tst' -TemplateFile .\2_StorageAccount.json -Verbose
+
+############ Task 1 - END########################################################################
+
+
+############# Task 2 ############################################################################
+###  Deploy Simple VM
+############# Task 2 ############################################################################
+
+#Create Resource Group for Simple VM
+New-AzureRmResourceGroup -Name "simpleVM-tst" -Location "WestEurope"
+
+#Deploy Simple VM from template
+New-AzureRmResourceGroupDeployment -ResourceGroupName 'simpleVM-tst' -TemplateFile .\simple-Windows-VM\azuredeploy.json `
+    -TemplateParameterFile .\simple-Windows-VM\azuredeploy.parameters.json `
+    -Name "deployStorage-$(Get-Date -Format 'yyyMMddhhmmss')" -Verbose
+
+############ Task 2 - END########################################################################
+
 
 
 
---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Exercises 
+1. [Accessing and managing Azure](Accessing%20and%20Managing%20Azure.md)
+1. [Create the policy for Enterprise IT](https://github.com/Microsoft/MCW-Enterprise-ready-cloud/blob/master/Hands-on%20lab/HOL%20step-by-step%20-%20Enterprise-ready%20cloud.md#solution-architecture)
+1. [Configure delegated permissions](https://github.com/Microsoft/MCW-Enterprise-ready-cloud/blob/master/Hands-on%20lab/HOL%20step-by-step%20-%20Enterprise-ready%20cloud.md#exercise-2-configure-delegated-permissions)
+1. [Identity](Accessing%20and%20Managing%20Azure.md)
